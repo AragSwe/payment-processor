aea4b8133f8535a466f93e0174ebb3562fd25567
Mikael Dahlqvist <zeractor@gmail.com>
2022-09-22 22:54:11 +0200

    init consumer

---
+++ payment-consumer/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using Microsoft.Extensions.Hosting;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+using IHost host = Host.CreateDefaultBuilder(args)
+    .Build();
+
+Task.Delay(10*1000).Wait();
+
+var factory = new ConnectionFactory { HostName = "payment-consumer-rabbit" };
+using (var connection = factory.CreateConnection())
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(
+        queue: "incoming",
+        durable: false,
+        exclusive: false,
+        autoDelete: false,
+        arguments: null
+    );
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (_, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine($" Consumed {message}");
+    };
+
+    channel.BasicConsume(
+        queue: "incoming",
+        autoAck: true,
+        consumer: consumer
+    );
+
+    await host.RunAsync();
+}
